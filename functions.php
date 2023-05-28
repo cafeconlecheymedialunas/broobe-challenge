d49f4944ebebd4fe6cfa2e43c75eefedd75fd85b
Mauro Gaitan <maurodeveloper86@gmail.com>
2023-05-28 19:16:56 -0300

    Add dummy data and constants

--- functions.php
+++ functions.php
@@ -3,7 +3,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 define('ASSETS_VERSION', '1.0.1');
 define("TEXT_DOMAIN","broobe-challenge");
-
+define("CUSTOM_ENDPOINT_NAMESPACE",'custom/v1');
+define("PAGINATION_DEFAULT_PAGE",1);
+define("PAGINATION_DEFAULT_PER_PAGE",10);
 use Src\Theme;
 
 
